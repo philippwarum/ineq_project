62139a6b5a870caff7c00739ddd07143b80be947
Wabnegger94 <i.wabnegger@gmx.net>
2019-01-19 12:51:15 +0000

    indicators on country level

--- R/eu28_wikipedia.R
+++ R/eu28_wikipedia.R
@@ -30,6 +30,18 @@ S80_20.EU <- filter(df, indic_il =="S80_S20", time >="2016", sex=="T", age=="TOT
 mean.EU <- filter(df, indic_il=="MEI_E", time >="2016",unit=="EUR", sex=="T", age=="TOTAL")
 
 
+indicators.cl <- bind_cols(gini.EU, median.EU, mean.EU, S80_20.EU)
+colnames(indicators.cl)[colnames(indicators.cl)=="values"] <- "gini"
+colnames(indicators.cl)[colnames(indicators.cl)=="values1"] <- "median" 
+colnames(indicators.cl)[colnames(indicators.cl)=="values2"] <- "mean"
+colnames(indicators.cl)[colnames(indicators.cl)=="values3"] <- "S80_20"
+
+indicators.cl <- select(indicators.cl, geo, time, gini, median, mean, S80_20)
+
+indicators.cl <- filter(indicators.cl, geo%in% c("AT", "BE", "BG", "CY", "CZ", "DE", "DK", 
+                                                 "EE", "EL", "ES","FI", "FR", "HR", 
+                                                 "HU", "IE", "IT", "LT", "LU", "LV", "MT", 
+                                                 "NL", "PL", "PT", "RO", "SE", "SI", "SK", "UK", "EU28"))
 
 # get data
 gini <- get_eurostat(id = "ilc_di12", time_format = "num")
